6c3e2240c650f0e367293b884d7b0412a3b3873a
Emmanuel Sales <hungbruno@gmail.com>
2025-09-04 14:16:01 -0300

    aula01 finalizada

--- aula01/src/App.jsx
+++ aula01/src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import Adicao from './components/Adicao'
-import Subtracao from './components/Subtracao'
-import Multiplicacao from './components/Multiplicacao'
-import Divisao from './components/Divisao'
+import PrecisoEstudar from './components/PrecisoEstudar'
 
 const App = () => {
   return (
     <div>
-      <h3>Primeiro número = 2</h3>
-      <h3>Segundo número = 2</h3>
-      <Adicao num1={2} num2={2} />
-      <Subtracao num1={2} num2={2} />
-      <Multiplicacao num1={2} num2={2} />
-      <Divisao num1={2} num2={2} />
+
+      <PrecisoEstudar nomeDaTecnologia="React" />
     </div>
   )
 }
