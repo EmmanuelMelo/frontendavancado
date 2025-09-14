3d31ae4428fb486ef28e1bed0e7a9f60b15ae8b9
Emmanuel Sales <hungbruno@gmail.com>
2025-09-14 15:40:51 -0300

    aula03 - if ternario.

---
+++ aula03/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EstouConseguindoAprenderReact from './assets/components/EstouConseguindoAprenderReact'
+
+const App = () => {
+  return (
+    <div>
+      <EstouConseguindoAprenderReact estouConseguindo={false} />
+    </div>
+  )
+}
+
+export default App
