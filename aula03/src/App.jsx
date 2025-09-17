85cff9b0b7e89a6e5b5cf06c708410a61b91eb3c
Emmanuel Sales <hungbruno@gmail.com>
2025-09-17 20:42:55 -0300

    Novas props

--- aula03/src/App.jsx
+++ aula03/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import EstaComSono from './assets/components/EstaComSono'
+import Aluno from './assets/components/Aluno'
 
 const App = () => {
   return (
     <div>
-      <EstaComSono estaComSono={false} />
+      {
+        [
+          {nome: 'Ronnie James Dio', email: 'dio@gmail.com', curso: 'Musica', media: 5.5},
+          {nome: 'Sol M. S.', email: 'solms@gmail.com', curso: 'Quimica', media: 9.5},
+          {nome: 'Liz M S', email: 'lizms@gmail.com', curso: 'Física', media: 10}
+        ].map((aluno) => 
+          <Aluno nome={aluno.nome} email={aluno.email} curso={aluno.curso} media={aluno.media}/>
+        )
+      }
     </div>
   )
 }
