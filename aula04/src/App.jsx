642b6d35ca09c958ecd9fc02f1ec8c35e3d7925b
Emmanuel Sales <hungbruno@gmail.com>
2025-09-24 19:57:22 -0300

    aula04 exercicio imagem.

---
+++ aula04/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Imagens from './components/Imagens'
+
+const App = () => {
+  return (
+    <>
+      <Imagens />
+    </>
+  )
+}
+
+export default App
---
+++ aula04/src/components/Imagens.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Imagens = () => {
+  return (
+    <>
+        <img src='jake.png' alt="Jake" /><br/>
+        Legenda da foto.
+    </>
+  )
+}
+
+export default Imagens
